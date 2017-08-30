05259b6354a140f393a32dec67fa817f2c5fc7fa
Samantha Martorana <samanthamartorana7@gmail.com>
2017-08-30 15:26:31 -0500

    Initial commit

--- client.js
+++ client.js
@@ -10,3 +10,48 @@ var employees = [ atticus, jem, boo, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log(employees);
+
+function calculatePercentage(employee) {
+
+    var bonus = 0;
+    var rating = employee.reviewRating;
+    var number = employee.employeeNumber;
+    var income = employee.annualSalary;
+
+    if (rating <= 2) {
+        bonus = 0;
+    } else if (rating === 3) {
+        bonus = 0.04;
+    } else if (rating === 4) {
+        bonus = 0.06;
+    } else if (rating === 5) {
+        bonus = 0.10;
+    }
+
+    if (number.length === 4) {
+        bonus += 0.05;
+    }
+
+    if (income > 65000) {
+        bonus -= 0.01;
+    }
+
+    if (bonus > 0.13) {
+        bonus = 0.13;
+    }
+
+    if (bonus < 0)  {
+        bonus = 0;
+    }
+    return bonus;
+}
+console.log(calculatePercentage(jem));
+
+function calculateTotalComp(employee, bonus){
+    var bonusAmount = parseInt(employee.annualSalary) * bonus;
+    console.log(bonusAmount);
+    var totalComp = parseInt(employee.annualSalary) + bonusAmount;
+    return totalComp;
+}
+console.log(calculateTotalComp(jem, 0.06));
+
